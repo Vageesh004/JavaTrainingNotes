e224ffa5ca2cce6617c2c92b15bdb1a2c25b82ed
Vageesh004 <138150465+Vageesh004@users.noreply.github.com>
2025-12-05 08:08:59 +0530

    Update Property.java

--- JavaProject/Property.java
+++ JavaProject/Property.java
@@ -3,47 +3,74 @@ package com.project2.CoreJavaProj;
 
 public class Property{
 
+	private static int idCounter=1;
+	
     private int propertyId;
     private double builtUpArea;
     private double baseValue;
-    private boolean inCity;
+    private boolean isCity;
     private int ageOfConstruction;
     private String locality;
+    private double propertyTax;
 
-    public Property(int propertyId, double builtUpArea, double baseValue,
-                    boolean inCity, int ageOfConstruction, String locality){
+    public Property() {
+    	super();
+    	
+    }
+    public Property(double builtUpArea, double baseValue,
+                    boolean inCity, int ageOfConstruction, String locality,double propertyTax){
 
-        this.propertyId = propertyId;
+        this.propertyId = idCounter++;
         this.builtUpArea = builtUpArea;
         this.baseValue = baseValue;
-        this.inCity = inCity;
+        this.isCity = isCity;
         this.ageOfConstruction = ageOfConstruction;
         this.locality = locality;
+        this.propertyTax=propertyTax;
     }
+	
+    
+	public void setPropertyId(int propertyId) {
+		this.propertyId = propertyId;
+	}
+	public double getBuiltUpArea() {
+		return builtUpArea;
+	}
+	public void setBuiltUpArea(double builtUpArea) {
+		this.builtUpArea = builtUpArea;
+	}
+	public double getBaseValue() {
+		return baseValue;
+	}
+	public void setBaseValue(double baseValue) {
+		this.baseValue = baseValue;
+	}
+	public boolean isCity() {
+		return isCity;
+	}
+	public void setCity(boolean isCity) {
+		this.isCity = isCity;
+	}
+	public int getAgeOfConstruction() {
+		return ageOfConstruction;
+	}
+	public void setAgeOfConstruction(int ageOfConstruction) {
+		this.ageOfConstruction = ageOfConstruction;
+	}
+	public String getLocality() {
+		return locality;
+	}
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+	public double getPropertyTax() {
+		return propertyTax;
+	}
+	public void setPropertyTax(double propertyTax) {
+		this.propertyTax = propertyTax;
+	}
 
-    public int getPropertyId() {
-        return propertyId;
-    }
 
-    public double getBuiltUpArea() {
-        return builtUpArea;
-    }
-
-    public double getBaseValue() {
-        return baseValue;
-    }
-
-    public boolean isInCity() {
-        return inCity;
-    }
-
-    public int getAgeOfConstruction() {
-        return ageOfConstruction;
-    }
-
-    public String getLocality() {
-        return locality;
-    }
     
     
 }
